97ee9deba8363d99ec4ad3267daac3e9b4eb1da9
Arihant Bapna <greatbapna@gmail.com>
2021-04-23 19:43:49 -0400

    Update CRUD

--- server.js
+++ server.js
@@ -84,10 +84,70 @@ app.get("/data", function (req, res) {
     x.forEach(function (i) {
       i.catName = getString(parseInt(i.category));
     });
-    res.render("pages/data", { root: __dirname, data: x, page_name: 'data' });
+    res.render("pages/data", { root: __dirname, data: x, page_name: "data" });
   });
 });
 //End of data page
+
+//CRUD Operations on questions table
+app.post("/handle", function (req, res) {
+  const dat = req.body;
+  pool.query(
+    "INSERT INTO questions (key, question, category) VALUES ($1, $2, $3)",
+    [dat.key, dat.Question, dat.category],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+    }
+  );
+  res.redirect("/");
+});
+
+app.post("/handler", function (req, res) {
+  const dat = req.body;
+  pool.query(
+    "INSERT INTO questions (key, question, category, type, approval) VALUES ($1, $2, $3, $4, $5)",
+    [dat.key, dat.Question, dat.category, dat.type, 0],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+    }
+  );
+  res.redirect("/");
+});
+
+app.post("/update", function (req, res) {
+  const dat = req.body;
+  console.log(dat.idHold);
+  console.log(dat.Question);
+  pool.query(
+    "UPDATE questions SET question = $1, approval = $2, category = $3 WHERE id = $4",
+    [dat.Question, dat.App, dat.category, dat.idHold],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+    }
+  );
+  res.redirect("/data");
+});
+
+app.post("/delete", function (req, res) {
+  const dat = req.body;
+  pool.query(
+    "DELETE FROM questions WHERE id = $1",
+    [dat.deleteId],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+    }
+  );
+  res.redirect("/data");
+});
+//end CRUD on questions
 
 // writing page page
 app.get("/writer", function (req, res) {
@@ -176,193 +236,193 @@ app.post("/generateQuestion", function (req, res) {
 
 //obscure functions
 function getNumbers(num) {
-    switch (num) {
-      //Science
-      case 1:
-        return 14;
-        break;
-      case 2:
-        return 14;
-        break;
-      case 3:
-        return 14;
-        break;
-      case 4:
-        return 7;
-        break;
-      case 5:
-        return 2;
-        break;
-      case 6:
-        return 3;
-        break;
-      case 7:
-        return 2;
-        break;
-      //Literature
-      case 8:
-        return 18;
-        break;
-      case 9:
-        return 10;
-        break;
-      case 10:
-        return 14;
-        break;
-      case 11:
-        return 7;
-        break;
-      case 12:
-        return 7;
-        break;
-      case 13:
-        //Fine arts
-        return 14;
-        break;
-      case 14:
-        return 14;
-        break;
-      case 15:
-        return 14;
-        break;
-      //RMPSS
-      case 16:
-        return 14;
-        break;
-      case 17:
-        return 14;
-        break;
-      case 18:
-        return 7;
-        break;
-      case 19:
-        return 4;
-        break;
-      case 20:
-        return 3;
-        break;
-      case 21:
-        return 3;
-        break;
-      case 22:
-        return 14;
-        break;
-      case 23:
-        return 14;
-        break;
-      case 24:
-        return 7;
-        break;
-      case 25:
-        return 21;
-        break;
-      //Other
-      case 26:
-        return 9;
-        break;
-      case 27:
-        return 10;
-        break;
-      case 28:
-        return 9;
-        break;
-    }
+  switch (num) {
+    //Science
+    case 1:
+      return 14;
+      break;
+    case 2:
+      return 14;
+      break;
+    case 3:
+      return 14;
+      break;
+    case 4:
+      return 7;
+      break;
+    case 5:
+      return 2;
+      break;
+    case 6:
+      return 3;
+      break;
+    case 7:
+      return 2;
+      break;
+    //Literature
+    case 8:
+      return 18;
+      break;
+    case 9:
+      return 10;
+      break;
+    case 10:
+      return 14;
+      break;
+    case 11:
+      return 7;
+      break;
+    case 12:
+      return 7;
+      break;
+    case 13:
+      //Fine arts
+      return 14;
+      break;
+    case 14:
+      return 14;
+      break;
+    case 15:
+      return 14;
+      break;
+    //RMPSS
+    case 16:
+      return 14;
+      break;
+    case 17:
+      return 14;
+      break;
+    case 18:
+      return 7;
+      break;
+    case 19:
+      return 4;
+      break;
+    case 20:
+      return 3;
+      break;
+    case 21:
+      return 3;
+      break;
+    case 22:
+      return 14;
+      break;
+    case 23:
+      return 14;
+      break;
+    case 24:
+      return 7;
+      break;
+    case 25:
+      return 21;
+      break;
+    //Other
+    case 26:
+      return 9;
+      break;
+    case 27:
+      return 10;
+      break;
+    case 28:
+      return 9;
+      break;
   }
-  
-  function getString(num) {
-    switch (num) {
-      //Science
-      case 1:
-        return "Biology";
-        break;
-      case 2:
-        return "Chemistry";
-        break;
-      case 3:
-        return "Physics";
-        break;
-      case 4:
-        return "Math";
-        break;
-      case 5:
-        return "Astronomy";
-        break;
-      case 6:
-        return "Computer Science";
-        break;
-      case 7:
-        return "Earth, Ocean and Atmospheric Science";
-        break;
-      //Literature
-      case 8:
-        return "Long Fiction";
-        break;
-      case 9:
-        return "Drama";
-        break;
-      case 10:
-        return "Poetry";
-        break;
-      case 11:
-        return "Short Fiction";
-        break;
-      case 12:
-        return "Other";
-        break;
-      case 13:
-        //Fine arts
-        return "Painting and Sculpture";
-        break;
-      case 14:
-        return "Classical Music";
-        break;
-      case 15:
-        return "Other";
-        break;
-      //RMPSS
-      case 16:
-        return "Religion";
-        break;
-      case 17:
-        return "Mythology";
-        break;
-      case 18:
-        return "Philosophy";
-        break;
-      case 19:
-        return "Econ";
-        break;
-      case 20:
-        return "Psych";
-        break;
-      case 21:
-        return "Other";
-        break;
-      //History
-      case 22:
-        return "US History";
-        break;
-      case 23:
-        return "European History";
-      case 24:
-        return "European Ancient";
-      case 25:
-        return "World History";
-      //Other
-      case 26:
-        return "Geography";
-        break;
-      case 27:
-        return "Popular Culture";
-        break;
-      case 28:
-        return "Current Events";
-        break;
+}
+
+function getString(num) {
+  switch (num) {
+    //Science
+    case 1:
+      return "Biology";
+      break;
+    case 2:
+      return "Chemistry";
+      break;
+    case 3:
+      return "Physics";
+      break;
+    case 4:
+      return "Math";
+      break;
+    case 5:
+      return "Astronomy";
+      break;
+    case 6:
+      return "Computer Science";
+      break;
+    case 7:
+      return "Earth, Ocean and Atmospheric Science";
+      break;
+    //Literature
+    case 8:
+      return "Long Fiction";
+      break;
+    case 9:
+      return "Drama";
+      break;
+    case 10:
+      return "Poetry";
+      break;
+    case 11:
+      return "Short Fiction";
+      break;
+    case 12:
+      return "Other";
+      break;
+    case 13:
+      //Fine arts
+      return "Painting and Sculpture";
+      break;
+    case 14:
+      return "Classical Music";
+      break;
+    case 15:
+      return "Other";
+      break;
+    //RMPSS
+    case 16:
+      return "Religion";
+      break;
+    case 17:
+      return "Mythology";
+      break;
+    case 18:
+      return "Philosophy";
+      break;
+    case 19:
+      return "Econ";
+      break;
+    case 20:
+      return "Psych";
+      break;
+    case 21:
+      return "Other";
+      break;
+    //History
+    case 22:
+      return "US History";
+      break;
+    case 23:
+      return "European History";
+    case 24:
+      return "European Ancient";
+    case 25:
+      return "World History";
+    //Other
+    case 26:
+      return "Geography";
+      break;
+    case 27:
+      return "Popular Culture";
+      break;
+    case 28:
+      return "Current Events";
+      break;
     default:
-        return "Something";
-    }
+      return "Something";
   }
-  //end obscure functions
+}
+//end obscure functions
 
 async function selectClues(m) {
   try {
@@ -375,8 +435,6 @@ async function selectClues(m) {
   }
 }
 //done with all things writing
-
-
 
 app.listen(80);
 console.log("80 is the magic port");
