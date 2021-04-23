695de4369a0a5f0358c9d6d86b685fcd29fb3aa3
Arihant Bapna <greatbapna@gmail.com>
2021-04-23 18:11:28 -0400

    Added Index page

--- server.js
+++ server.js
@@ -24,6 +24,54 @@ app.set('view engine', 'ejs');
 
 var db = [];
 
+
+//Index page
+app.get("/", function (req, res) {
+    pool.query(
+      "SELECT * FROM questions",
+      (error, results) => {
+        if (error) {
+          throw error;
+        }
+        var drim = [];
+        var drim2 = [];
+        var tLen = 0;
+        var bLen = 0;
+        var x = results.rows;
+        for (var i = 1; i <= 28; i++) {
+          var obj = { cat: "", val: 0 };
+          obj.cat = getString(i);
+          obj.tot = getNumbers(i);
+          obj.type = 'T';
+          for (var j = 0; j < x.length; j++) {
+            if (x[j].category == i && x[j].type === 'T') {
+              obj.val++;
+              tLen++;
+            }
+          }
+          drim.push(obj);
+        }
+  
+        
+  
+        for (var i = 1; i <= 28; i++) {
+          var obj = { cat: "", val: 0 };
+          obj.cat = getString(i);
+          obj.tot = getNumbers(i);
+          obj.type = 'B';
+          for (var j = 0; j < x.length; j++) {
+            if (x[j].category == i && x[j].type === 'B') {
+              obj.val++;
+              bLen++;
+            }
+          }
+          drim2.push(obj);
+        }
+        res.render("pages/index", { root: __dirname, data: drim, bonus: drim2, totalT: tLen, totalB: bLen })
+      });
+  });
+//End index page
+
 // writing page page
 app.get('/writer', function(req, res) {
     pool.query("SELECT * FROM answers", (error, results) => {
@@ -84,6 +132,194 @@ async function selectClues(m){
     }
 }
 //done with all things writing
+
+//obscure functions
+function getNumbers(num){
+    switch(num){
+      //Science
+      case 1:
+        return 14;
+        break;
+      case 2:
+        return 14;
+        break;
+      case 3:
+        return 14;
+        break;
+      case 4:
+        return 7;
+        break;
+      case 5:
+        return 2;
+        break;
+      case 6:
+        return 3;
+        break;
+      case 7:
+        return 2;
+        break;
+      //Literature
+      case 8:
+        return 18;
+        break;
+      case 9:
+        return 10;
+        break;
+      case 10:
+        return 14;
+        break;
+      case 11:
+        return 7;
+        break;
+      case 12:
+        return 7;
+        break;
+      case 13:
+      //Fine arts
+        return 14;
+        break;
+      case 14:
+        return 14;
+        break;
+      case 15:
+        return 14;
+        break;
+      //RMPSS
+      case 16:
+        return 14;
+        break;
+      case 17:
+        return 14;
+        break;
+      case 18:
+        return 7;
+        break;
+      case 19:
+        return  4;
+        break;
+      case 20:
+        return 3;
+        break;
+      case 21:
+        return 3;
+        break;
+      case 22:
+        return 14;
+        break;
+      case 23:
+        return 14;
+        break;
+      case 24:
+        return 7;
+        break;
+      case 25:
+        return 21;
+        break;
+      //Other
+      case 26:
+        return 9;
+        break;
+      case 27:
+        return 10;
+        break;
+      case 28:
+        return 9;
+        break;
+    }
+  }
+  
+function getString(num) {
+switch (num) {
+    //Science
+    case 1:
+    return "Biology";
+    break;
+    case 2:
+    return "Chemistry";
+    break;
+    case 3:
+    return "Physics";
+    break;
+    case 4:
+    return "Math";
+    break;
+    case 5:
+    return "Astronomy";
+    break;
+    case 6:
+    return "Computer Science";
+    break;
+    case 7:
+    return "Earth, Ocean and Atmospheric Science";
+    break;
+    //Literature
+    case 8:
+    return "Long Fiction";
+    break;
+    case 9:
+    return "Drama";
+    break;
+    case 10:
+    return "Poetry";
+    break;
+    case 11:
+    return "Short Fiction";
+    break;
+    case 12:
+    return "Other";
+    break;
+    case 13:
+    //Fine arts
+    return "Painting and Sculpture";
+    break;
+    case 14:
+    return "Classical Music";
+    break;
+    case 15:
+    return "Other";
+    break;
+    //RMPSS
+    case 16:
+    return "Religion";
+    break;
+    case 17:
+    return "Mythology";
+    break;
+    case 18:
+    return "Philosophy";
+    break;
+    case 19:
+    return "Econ";
+    break;
+    case 20:
+    return "Psych";
+    break;
+    case 21:
+    return "Other";
+    break;
+    //History
+    case 22:
+    return "US History";
+    break;
+    case 23:
+    return "European History";
+    case 24:
+    return "European Ancient";
+    case 25:
+    return "World History"
+    //Other
+    case 26:
+    return "Geography";
+    break;
+    case 27:
+    return "Popular Culture";
+    break;
+    case 28:
+    return "Current Events";
+    break;
+}
+}
+//end obscure functions
 
 app.listen(80);
 console.log('80 is the magic port');
